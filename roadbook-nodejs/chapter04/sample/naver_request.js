52be5609ff173c1bc2b35290f8bae00e4af14fb4
mucollabo <mucollabo@gmail.com>
2022-05-30 15:20:18 +0900

    add naver_request.js

---
+++ roadbook-nodejs/chapter04/sample/naver_request.js
@@ -0,0 +1,55 @@
+const morgan = require('morgan');
+const request = require('request');
+const express = require('express');
+const app = express();
+
+// setting port
+app.set('port', process.env.PORT || 8080);
+
+// common middleware
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// setting routing
+app.get('/search/blog', (req, res) => {
+    const client_id = 'MlijIwuqG_buu9KEuqe0';
+    const client_secret = '3YPEWWF7XD';
+    const api_url = 'https://openapi.naver.com/v1/search/blog?query=' + encodeURI('코스피'); //encodeURI(req.query.query);
+    const option = {
+    };
+    const options = {
+        url: api_url,
+        qs: option,
+        Headers: { 'X-Naver-Client-Id': client_id, 'X-Naver-Client-Secret': client_secret },
+    };
+
+    request.get(options, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            let newsItem = JSON.parse(body).items;       // items - title, link, description, pubDate
+
+            const newsJson = {
+                title: [],
+                link: [],
+                description: [],
+                pubDate: []
+            }
+
+            for (let i = 0; i < newsItem.length; i++) {
+                newsJson.title.push(newsItem[i].title.replace(/(<([^>]+)>)|&quot;/ig, ""));
+                newsJson.link.push(newsItem[i].link);
+                newsJson.description.push(newsItem[i].description.replace(/(<([^>]+)>)|&quot;/ig, ""));
+                newsJson.pubDate.push(newsItem[i].pubDate);
+            }
+            res.json(newsJson);
+        } else {
+            res.status(response.statusCode).end();
+            console.log('error = ' + response.statusCode);
+        }
+    });
+});
+
+// connecting between server and port
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 서버 실행 중 ..')
+});
